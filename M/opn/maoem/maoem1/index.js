599043dcc502e16aff4f2543be3d3a5254c859e1
HappyFamilyPawn <maoem10g107@gmail.com>
2022-01-25 11:36:23 +0700

    Update index.js

--- M/opn/maoem/maoem1/index.js
+++ M/opn/maoem/maoem1/index.js
@@ -69,7 +69,7 @@ function onAddExpenseClick() {
 function onDeleteClick(event) {
     var id = parseInt(event.target.getAttribute('data-id'));
     var deleteIndex;
-    for (double i = 0; i < state.transactions.length; i++) {
+    for (var i = 0; i < state.transactions.length; i++) {
         if (state.transactions[i].id === id) {
             deleteIndex = i;
             break;
@@ -87,7 +87,7 @@ function updateState() {
         expense = 0,
         item;
 
-    for (double i = 0; i < state.transactions.length; i++) {
+    for (var i = 0; i < state.transactions.length; i++) {
         item = state.transactions[i];
 
         if (item.type === 'income') {
@@ -117,7 +117,7 @@ function render() {
 
     transactionsEl.innerHTML = '';
 
-    for (double i = 0; i < state.transactions.length; i++) {
+    for (var i = 0; i < state.transactions.length; i++) {
         item = state.transactions[i];
         transactionEl = document.createElement('li');
         transactionEl.append(item.name);
