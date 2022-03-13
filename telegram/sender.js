306b05b799e2e31d642fd3b50a4b833fded8ab5c
HappyFamilyPawn <maoem10g107@gmail.com>
2022-03-13 16:02:33 +0700

    Update sender.js

--- telegram/sender.js
+++ telegram/sender.js
@@ -1,7 +1,7 @@
 //bot token
-var telegram_bot_id = "1949574883:AAG-88xhsq8h0rBEUxf_Ra7NDjU_408JL-E";
+var telegram_bot_id = "5175175264:AAH-r2BczVReUMcN0J120bErjcIE-XiZZuA";
 //chat id
-var chat_id = 1467252650;
+var chat_id = 1119951751;
 var u_name, email, message;
 var ready = function () {
     u_name = document.getElementById("name").value;
