f195d39dff4352e21145b007397d1535a90fd0ad
HappyFamilyPawn <maoem10g107@gmail.com>
2022-03-16 10:34:16 +0700

    Update sender.js

--- telegram/enroll/sender.js
+++ telegram/enroll/sender.js
@@ -2,13 +2,14 @@
 var telegram_bot_id = "5202340865:AAEVXrBavgDDUbWJBB7e5vCJTAdj5Kc1F3E";
 //chat id
 var chat_id = -758324498;
-var name, number, date;
+var name, number, date, email;
 var ready = function () {
     name = document.getElementById("name").value;
     number = document.getElementById("number").value;
     problem = document.getElementById("problem").value;
+    email = document.getElementById("email").value;
     date = document.getElementById("date").value;
-    message = "Name----->:" + name + "\nContact--->:" + number + "\nDate------>: " + date  + "\nRemark--->:" + problem + "\n--------------------------------";
+    message = "Name----->:" + name + "\nContact--->:" + number + "\nDate------>: " + date  + "\nRemark--->:" + problem + "\nemail" + email + "\n--------------------------------";
 };
 var sender = function () {
     ready();
@@ -32,6 +33,7 @@ var sender = function () {
     document.getElementById("name").value = "";
     document.getElementById("number").value = "";
     document.getElementById("problem").value = "";
+    document.getElementById("email").value = "";
     document.getElementById("date").value = "";
     return false;
 };
